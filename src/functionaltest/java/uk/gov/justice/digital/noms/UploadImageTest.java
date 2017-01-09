3b080e3df5bbf070ea7cc2c34d94883ce6000764
Nick Redshaw <nickredshaw@gmail.com>
2017-01-09 16:09:18 +0000

    Read the application hostname and port from env vars during functional test startup

--- src/functionaltest/java/uk/gov/justice/digital/noms/UploadImageTest.java
+++ src/functionaltest/java/uk/gov/justice/digital/noms/UploadImageTest.java
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.noms;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
@@ -11,10 +12,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UploadImageTest {
 
+    private String hostname;
+    private String port;
+
+    @Before
+    public void readHostnameAndPort() {
+        hostname = System.getenv("APPLICATION_HOSTNAME");
+        if (hostname == null || hostname.isEmpty()) {
+            hostname = "localhost";
+        }
+
+        port = System.getenv("APPLICATION_PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080";
+        }
+    }
+
     @Test
     public void uploadsImageAndTitleSuccessfully() throws Exception {
-
-        HttpResponse<String> response = Unirest.post("http://localhost:8080/content-items")
+        HttpResponse<String> response = Unirest.post("http://" + hostname + ":" + port +"/content-items")
                 .header("accept", "application/json")
                 .field("title", "A one pixel image")
                 .field("file", new File(this.getClass().getResource("/1-pixel.png").toURI()))
