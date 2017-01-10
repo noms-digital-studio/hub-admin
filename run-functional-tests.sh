4f3a5c113a236eb725725d867b31a47b80fbf63a
Nick Redshaw <nickredshaw@gmail.com>
2017-01-10 15:22:29 +0000

    Use gradle build and fix the find command

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -3,6 +3,6 @@
 STATUS_CODE="$?"
 set -e
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/
-find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \
+find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
 cp -r build/reports/tests/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report
 exit $STATUS_CODE
