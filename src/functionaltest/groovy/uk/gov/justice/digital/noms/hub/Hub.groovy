9c5032d81b25ee7020306e31997835f1db5e3d06
Alistair Todd <atodd917@yahoo.co.uk>
2017-02-16 09:07:16 +0000

    Spockify functional tests

---
+++ src/functionaltest/groovy/uk/gov/justice/digital/noms/hub/Hub.groovy
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.noms.hub
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class Hub {
+
+    String adminUri
+    String adminUiUri
+    String username
+    String password
+
+    Hub() {
+        username = System.getenv('BASIC_AUTH_USERNAME') ?: 'user'
+        password = System.getenv('BASIC_AUTH_PASSWORD') ?: 'password'
+
+        adminUri = (System.getenv('HUB_ADMIN_URI') ?: "http://localhost:8080/hub-admin")
+        adminUri = adminUri.replaceFirst('^https?://', "http://${username}:${password}@")
+        log.info("adminUri: ${adminUri}")
+
+        adminUiUri = (System.getenv('HUB_ADMIN_UI_URI') ?: "http://localhost:3000/")
+        adminUiUri = adminUiUri.replaceFirst('^https?://', "http://${username}:${password}@")
+        log.info("adminUiUri: ${adminUiUri}")
+    }
+}
