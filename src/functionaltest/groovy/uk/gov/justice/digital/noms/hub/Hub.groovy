c62f9fda4584f1c38b7040b65a07a3c031364c93
Alistair Todd <atodd917@yahoo.co.uk>
2017-02-27 13:44:25 +0000

    Spockify functional test

--- src/functionaltest/groovy/uk/gov/justice/digital/noms/hub/Hub.groovy
+++ src/functionaltest/groovy/uk/gov/justice/digital/noms/hub/Hub.groovy
@@ -6,10 +6,11 @@ import groovy.util.logging.Slf4j
 class Hub {
 
     String adminUri
-    String adminUiUri
     String username
     String password
     String mongoConnectionUri
+    String azureConnectionUri
+    String azurePublicUrlBase
 
     Hub() {
         username = System.getenv('BASIC_AUTH_USERNAME') ?: 'user'
@@ -19,5 +20,9 @@ class Hub {
         adminUri = adminUri.replaceFirst('^https?://', "http://${username}:${password}@")
 
         mongoConnectionUri = System.getenv('MONGODB_CONNECTION_URI') ?: 'mongodb://localhost:27017'
+
+        azureConnectionUri = System.getenv("AZURE_BLOB_STORE_CONNECTION_URI") ?: 'AccountName=abc;AccountKey=YWJjCg==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;';
+
+        azurePublicUrlBase = System.getenv("AZURE_BLOB_STORE_PUBLIC_URL_BASE") ?: 'http://127.0.0.1:10000';
     }
 }
---
+++ src/functionaltest/groovy/uk/gov/justice/digital/noms/hub/UploadFileSpec.groovy
@@ -0,0 +1,195 @@
+package uk.gov.justice.digital.noms.hub
+
+import com.gmongo.GMongo
+import com.gmongo.GMongoClient
+import com.mashape.unirest.http.Unirest
+import com.microsoft.azure.storage.CloudStorageAccount
+import com.microsoft.azure.storage.blob.*
+import com.mongodb.DB
+import com.mongodb.MongoClientURI
+import groovy.json.JsonSlurper
+import org.bson.types.ObjectId
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.security.DigestInputStream
+import java.security.MessageDigest
+
+class UploadFileSpec extends Specification {
+
+    private static final String FILE_NAME = "hub-admin-1-pixel.png"
+    private static final String TITLE = "aTitle"
+    private static final String CATEGORY = "aCategory"
+    private static final String CONTENT_TYPE = "aMediaType"
+
+    @Shared
+    private Hub theHub
+
+    @Shared
+    private String adminAppRoot
+
+    @Shared
+    private GMongo mongo
+
+    @Shared
+    private DB db
+
+    @Shared
+    private CloudBlobContainer container
+
+    private Map basicAuth
+    private Date startDate
+
+    def setup() {
+        theHub = new Hub()
+
+        adminAppRoot = theHub.adminUri
+
+        MongoClientURI mongoUri = new MongoClientURI(theHub.mongoConnectionUri)
+        mongo = new GMongoClient(mongoUri)
+        db = mongo.getDB("hub_metadata")
+
+        String credentials = "${theHub.username}:${theHub.password}".bytes.encodeBase64()
+        basicAuth = [requestProperties: [Authorization: "Basic ${credentials}"]]
+
+        startDate = new Date()
+
+        CloudStorageAccount storageAccount = CloudStorageAccount.parse(theHub.azureConnectionUri)
+        CloudBlobClient blobClient = storageAccount.createCloudBlobClient()
+        container = blobClient.getContainerReference('content-items')
+        container.createIfNotExists()
+
+        BlobContainerPermissions containerPermissions = new BlobContainerPermissions()
+        containerPermissions.setPublicAccess(BlobContainerPublicAccessType.CONTAINER)
+        container.uploadPermissions(containerPermissions)
+    }
+
+
+    def 'uploads a new image with title'() {
+
+        given:
+        deleteImageFromFileStore(FILE_NAME)
+        db.contentItem.remove(filename: FILE_NAME)
+
+        when:
+        def postResponse =
+                Unirest.post(adminAppRoot + '/content-items')
+                        .basicAuth(theHub.username, theHub.password)
+                        .field("file", getFile(FILE_NAME))
+                        .field("metadata", someJsonMetadata())
+                        .asJson()
+
+        then: 'the item is created'
+        postResponse.statusCode == 201
+        def locationHeader = postResponse.headers.getFirst('Location')
+        locationHeader.contains('/content-items/')
+
+        and: 'the data record is created'
+        def id = locationHeader.tokenize('/').last()
+        def itemData = db.contentItem.findOne(_id: new ObjectId(id))
+
+        with(itemData) {
+            filename == FILE_NAME
+            metadata.title == TITLE
+            metadata.category == CATEGORY
+            metadata.contentType == CONTENT_TYPE
+        }
+
+        and: 'the data record has a timestamp'
+        def timestamp = Date.parse("yyyy-MM-dd'T'HH:mm:ss.sss'Z'", itemData.timestamp)
+        timestamp.compareTo(startDate) >= 0
+
+        and: 'the data record contains the content uri'
+        itemData.files.main == "${theHub.azurePublicUrlBase}/content-items/${FILE_NAME}"
+
+        and: 'the content uri can be used to retrive the same file content'
+        def imageResponse = Unirest.get(itemData.files.main).asString()
+        imageResponse.status == 200
+        mD5For(imageResponse.getRawBody()) == mD5For(getFileStream(FILE_NAME))
+
+        cleanup:
+        deleteImageFromFileStore(FILE_NAME)
+        db.contentItem.remove(_id: new ObjectId(id))
+
+    }
+
+    def 'overwrites existing data record for same filename'() {
+
+        given:
+        putImageInFileStore(FILE_NAME)
+
+        db.contentItem.insert(
+                filename: "${FILE_NAME}",
+                metadata: [
+                        title      : "oldTitle",
+                        category   : "oldCategory",
+                        contentType: "oldContentType"
+                ]
+        )
+
+        assert db.contentItem.find(filename: FILE_NAME).count() == 1
+
+        when:
+        Unirest.post(adminAppRoot + '/content-items')
+                .basicAuth(theHub.username, theHub.password)
+                .field("file", getFile(FILE_NAME))
+                .field("metadata", someJsonMetadata())
+                .asJson()
+
+        then: 'there should only be one record with that filename'
+        db.contentItem.find(filename: FILE_NAME).count() == 1
+
+        and: 'the record should have the new metadata'
+        def itemData = db.contentItem.findOne(filename: FILE_NAME)
+
+        with(itemData) {
+            filename == FILE_NAME
+            metadata.title == TITLE
+            metadata.category == CATEGORY
+            metadata.contentType == CONTENT_TYPE
+        }
+
+        cleanup:
+        deleteImageFromFileStore(FILE_NAME)
+        db.contentItem.remove(filename: FILE_NAME)
+    }
+
+    def deleteImageFromFileStore(identifier) {
+        CloudBlockBlob blob = container.getBlockBlobReference(identifier)
+        blob.deleteIfExists()
+    }
+
+    def putImageInFileStore(identifier) {
+        CloudBlockBlob blob = container.getBlockBlobReference(identifier)
+        blob.upload(getFileStream(identifier), getFile(identifier).length())
+    }
+
+    def someJsonMetadata() {
+        return """
+        { 
+            "title": "${TITLE}", 
+            "category": "${CATEGORY}", 
+            "contentType": "${CONTENT_TYPE}",
+            "fileLabels": ["main"]
+        }
+        """
+    }
+
+    def getFileStream(filename) throws FileNotFoundException, URISyntaxException {
+        return new FileInputStream(getFile(filename))
+    }
+
+    def getFile(String filename) throws URISyntaxException {
+        return new File(this.getClass().getResource("/" + filename).toURI())
+    }
+
+    def mD5For(InputStream is) {
+        MessageDigest md = MessageDigest.getInstance("MD5")
+        DigestInputStream dis = new DigestInputStream(is, md)
+        while (dis.read() != -1) {
+        }
+        dis.close()
+        return md.digest()
+    }
+
+}
